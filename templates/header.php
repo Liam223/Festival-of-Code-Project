bbda83895756ba512b375b53be3a65969e2c1489
Gaz432 <gaz_man_432@overclocked.net>
2014-07-28 15:06:52 +0100

    Fixed title in header

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">[Insert name here]</a>
+					<a class="navbar-brand" href="/"><?php echo SITE_TITLE; ?></a>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
